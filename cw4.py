2ca7b6a2cee01fa9a8b4614c209ed70bb235c48b
Raghadbader <142684190+Raghadbader@users.noreply.github.com>
2023-09-02 19:54:41 +0300

    raghad

---
+++ cw4.py
@@ -0,0 +1,24 @@
+def my_name():
+    print("my name is raghad")
+
+my_name()
+
+def my_meal(food,drink):
+    print(f"I like to eat {food} and while drinking {drink}")
+    
+my_meal("appel","water")
+
+    
+def cube(number):
+    return number **3
+
+print(cube(3))
+
+def by_three(number):
+    if number %3==0:
+        return cube(number)
+    else:
+        return False
+
+print(by_three(9))
+ 
